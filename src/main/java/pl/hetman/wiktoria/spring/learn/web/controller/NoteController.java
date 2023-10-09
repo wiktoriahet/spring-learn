15d8bd421bded7d4248362c4f0336ba74ce6fc10
wiktoriahet <wiktoriaspyc@gmail.com>
2023-10-09 12:03:30 +0200

    added spring annotations and http protocol methods, created logger

--- src/main/java/pl/hetman/wiktoria/spring/learn/web/controller/NoteController.java
+++ src/main/java/pl/hetman/wiktoria/spring/learn/web/controller/NoteController.java
@@ -1,4 +1,38 @@
 package pl.hetman.wiktoria.spring.learn.web.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.hetman.wiktoria.spring.learn.web.model.NoteModel;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+@Controller
+@RequestMapping(value = "/notes")
 public class NoteController {
+    public static final Logger LOGGER = Logger.getLogger(NoteController.class.getName());
+    private List<NoteModel> notes = new ArrayList<>();
+
+    @GetMapping
+    public String list() {
+        LOGGER.info("list() = " + notes);
+        return "notes/notes";
+    }
+
+    @GetMapping(value = "/create")
+    public String createView() {
+        return "notes/create-note";
+    }
+
+    @PostMapping
+    public String create(NoteModel note) {
+        LOGGER.info("create(" + note + ")");
+        notes.add(note);
+        return "redirect:/notes";
+    }
+
+
 }
