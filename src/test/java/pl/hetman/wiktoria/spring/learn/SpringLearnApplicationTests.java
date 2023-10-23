cfc02e639c5f0122bb642909eabdb81511711be4
wiktoriahet <wiktoriaspyc@gmail.com>
2023-10-23 13:01:47 +0200

    refactored spring test

--- src/test/java/pl/hetman/wiktoria/spring/learn/SpringLearnApplicationTests.java
+++ src/test/java/pl/hetman/wiktoria/spring/learn/SpringLearnApplicationTests.java
@@ -1,13 +1,20 @@
 package pl.hetman.wiktoria.spring.learn;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import pl.hetman.wiktoria.spring.learn.app.bookstore.web.controller.BookController;
 
 @SpringBootTest
 class SpringLearnApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private BookController bookController;
 
+	@Test
+	void contextLoads() throws Exception {
+//		assertThat(bookController).isNotNull();
+		Assertions.assertNotNull(bookController, "bookController is null");
+	}
 }
