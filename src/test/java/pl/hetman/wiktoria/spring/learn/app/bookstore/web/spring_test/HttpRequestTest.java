fbfea27409d004c70e9a6bd1c15bd75d2eb12153
wiktoriahet <wiktoriaspyc@gmail.com>
2023-10-23 13:18:06 +0200

    created test for http request

---
+++ src/test/java/pl/hetman/wiktoria/spring/learn/app/bookstore/web/spring_test/HttpRequestTest.java
@@ -0,0 +1,28 @@
+package pl.hetman.wiktoria.spring.learn.app.bookstore.web.spring_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HttpRequestTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void greetingShouldReturnDefaultMessage() throws Exception {
+//        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
+//                String.class)).contains("Hello, World");
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
+                String.class)).isNotEmpty();
+    }
+
+}
