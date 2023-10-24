a669db2f79b2ff418cd337482ad9685299ddd478
wiktoriahet <wiktoriaspyc@gmail.com>
2023-10-24 14:12:38 +0200

    deleted lombok

--- src/main/java/pl/hetman/wiktoria/spring/learn/app/bookstore/web/model/BookModel.java
+++ src/main/java/pl/hetman/wiktoria/spring/learn/app/bookstore/web/model/BookModel.java
@@ -1,14 +1,38 @@
 package pl.hetman.wiktoria.spring.learn.app.bookstore.web.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-
-@ToString
-@NoArgsConstructor
-@AllArgsConstructor
 public class BookModel {
     private String title;
     private int pages;
+
+    public BookModel() {
+    }
+
+    public BookModel(String title, int pages) {
+        this.title = title;
+        this.pages = pages;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    @Override
+    public String toString() {
+        return "BookModel{" +
+                "title='" + title + '\'' +
+                ", pages=" + pages +
+                '}';
+    }
 }
