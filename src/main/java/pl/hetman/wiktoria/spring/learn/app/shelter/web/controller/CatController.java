8e1a2e9da2a531cffef55206120dd23da2141591
wiktoriahet <wiktoriaspyc@gmail.com>
2023-10-31 13:12:22 +0100

    created list for cats

--- src/main/java/pl/hetman/wiktoria/spring/learn/app/shelter/web/controller/CatController.java
+++ src/main/java/pl/hetman/wiktoria/spring/learn/app/shelter/web/controller/CatController.java
@@ -1,11 +1,14 @@
 package pl.hetman.wiktoria.spring.learn.app.shelter.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.hetman.wiktoria.spring.learn.app.shelter.web.model.CatModel;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 @Controller
@@ -13,8 +16,10 @@ import java.util.logging.Logger;
 public class CatController {
     public static final Logger LOGGER = Logger.getLogger(CatController.class.getName());
 
+    private List<CatModel> cats = new ArrayList<>();
+
     @GetMapping
-    public String read(CatModel catModel){
+    public String read(CatModel catModel) {
         LOGGER.info("read(" + catModel + ")");
         LOGGER.info("read(...)");
         return "app/shelter/cats.html";
@@ -28,9 +33,23 @@ public class CatController {
     }
 
     @PostMapping
-    public String create(CatModel catModel){
+    public String create(CatModel catModel) {
         LOGGER.info("create(" + catModel + ")");
+        cats.add(catModel);
         LOGGER.info("create(...)");
+        //return "app/shelter/cats.html";
+        return "redirect:/cats/list";
+    }
+
+    @GetMapping(value = "/list")
+    public String list(ModelMap modelMap) {
+        LOGGER.info("list()");
+//        List<CatModel> cats = List.of(
+//                new CatModel("Puszek", 745),
+//                new CatModel("Mruczek", 26)
+//        );
+        modelMap.addAttribute("cats", cats);
+        LOGGER.info("list(...)");
         return "app/shelter/cats.html";
     }
 }
