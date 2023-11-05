516aff006ffb01b2a3770ba52bcc87c1da1f9128
wiktoriahet <wiktoriaspyc@gmail.com>
2023-11-05 11:42:31 +0100

    created interface for repository

---
+++ src/main/java/pl/hetman/wiktoria/spring/learn/app/bookstore/web/repository/Repository.java
@@ -0,0 +1,17 @@
+package pl.hetman.wiktoria.spring.learn.app.bookstore.web.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface Repository <T>{
+    Optional<T> create(T t);
+
+    Optional<T> read(Long id);
+
+    Optional<T> update(T t);
+
+    boolean delete(T t);
+
+    List<T> list ();
+
+}
