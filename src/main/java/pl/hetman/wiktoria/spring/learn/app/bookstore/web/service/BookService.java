365571de5c3bbcd78365f19c4c5ca7e427bb1d05
wiktoriahet <wiktoriaspyc@gmail.com>
2023-11-05 11:57:58 +0100

    added create method in service

--- src/main/java/pl/hetman/wiktoria/spring/learn/app/bookstore/web/service/BookService.java
+++ src/main/java/pl/hetman/wiktoria/spring/learn/app/bookstore/web/service/BookService.java
@@ -1,8 +1,11 @@
 package pl.hetman.wiktoria.spring.learn.app.bookstore.web.service;
 
 import org.springframework.stereotype.Service;
+import pl.hetman.wiktoria.spring.learn.app.bookstore.web.exception.BookException;
+import pl.hetman.wiktoria.spring.learn.app.bookstore.web.model.BookModel;
 import pl.hetman.wiktoria.spring.learn.app.bookstore.web.repository.BookRepository;
 
+import java.util.Optional;
 import java.util.logging.Logger;
 
 @Service
@@ -15,6 +18,21 @@ public class BookService {
     public BookService(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
     }
+
+    public BookModel create(BookModel bookModel) throws BookException {
+        LOGGER.info("create("+ bookModel +")");
+
+        Optional<BookModel> optionalBookModel =
+                bookRepository.create(bookModel);
+
+        BookModel createdBookModel = optionalBookModel.orElseThrow(
+                ()->new BookException("Unable to create book")
+        );
+
+        LOGGER.info("create(...)");
+        return createdBookModel;
+    }
+
 
     public void read(){
         LOGGER.info("read()");
