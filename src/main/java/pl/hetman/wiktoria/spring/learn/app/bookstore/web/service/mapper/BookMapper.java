fb7301551d32e7b128cff5ce63186e982745daf6
wiktoriahet <wiktoriaspyc@gmail.com>
2023-11-27 12:53:43 +0100

    created model mapper to book mapper

--- src/main/java/pl/hetman/wiktoria/spring/learn/app/bookstore/web/service/mapper/BookMapper.java
+++ src/main/java/pl/hetman/wiktoria/spring/learn/app/bookstore/web/service/mapper/BookMapper.java
@@ -1,5 +1,6 @@
 package pl.hetman.wiktoria.spring.learn.app.bookstore.web.service.mapper;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 import pl.hetman.wiktoria.spring.learn.app.bookstore.repository.entity.BookEntity;
 import pl.hetman.wiktoria.spring.learn.app.bookstore.web.model.BookModel;
@@ -7,15 +8,19 @@ import pl.hetman.wiktoria.spring.learn.app.bookstore.web.model.BookModel;
 @Component
 public class BookMapper {
 
+    ModelMapper modelMapper = new ModelMapper();
+
     public BookEntity from(BookModel bookModel) {
 
-        BookEntity bookEntity = new BookEntity();
+        BookEntity mappedBookEntity = modelMapper.map(bookModel, BookEntity.class);
 
-        bookEntity.setId(bookModel.getId());
-        bookEntity.setTitle(bookModel.getTitle());
-        bookEntity.setPages(bookModel.getPages());
+//        BookEntity bookEntity = new BookEntity();
 
-        return bookEntity;
+//        bookEntity.setId(bookModel.getId());
+//        bookEntity.setTitle(bookModel.getTitle());
+//        bookEntity.setPages(bookModel.getPages());
+
+        return mappedBookEntity;
     }
 // TODO: 23.11.2023 zamienić implementację metody from na użycie ModelMapper
     //https://modelmapper.org/
