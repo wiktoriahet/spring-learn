59b93df9918a55d00d18e31dd721ed35f3847362
wiktoriahet <wiktoriaspyc@gmail.com>
2023-12-28 11:03:31 +0100

    added web mock test for controller

--- src/test/java/pl/hetman/wiktoria/spring/learn/app/bookstore/web/spring_test/HttpRequestTest.java
+++ src/test/java/pl/hetman/wiktoria/spring/learn/app/bookstore/web/spring_test/HttpRequestTest.java
@@ -6,10 +6,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class HttpRequestTest {
+public class HttpRequestTest { //jakbym otwierała stronę przez przeglądarkę
+    //To do that, you could start the application
+    // and listen for a connection (as it would do in production)
+    // and then send an HTTP request and assert the response.
 
     @LocalServerPort
     private int port;
@@ -18,10 +19,17 @@ public class HttpRequestTest {
     private TestRestTemplate restTemplate;
 
     @Test
-    void bookControllerHttpRequest() throws Exception {
-        assertThat(this.restTemplate.getForObject(
-                "http://localhost:" + port + "/books",
-                String.class)).contains("bookstore");
+    void bookControllerHttpRequest() {
+//        try {
+            this.restTemplate.getForObject(
+                    "http://localhost:" + port + "/books?id=-99",
+                    String.class).contains("bookstore");
+//            System.out.println("#### NO EXCEPTION!!!");
+//            Assertions.assertTrue(true);
+//        } catch (Exception e) {
+//            System.out.println("#### EXCEPTION!!!");
+//            Assertions.assertTrue(false);
+//        }
     }
 
 }
--- src/test/java/pl/hetman/wiktoria/spring/learn/app/bookstore/web/spring_test/TestWebAppMock.java
+++ src/test/java/pl/hetman/wiktoria/spring/learn/app/bookstore/web/spring_test/TestWebAppMock.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -15,17 +16,29 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class TestWebAppMock {
-
+public class TestWebAppMock { //bez serwera, cały kontekst aplikacji springowej
+//Another useful approach is to not start the server
+// at all but to test only the layer below that, where
+// Spring handles the incoming HTTP request and
+// hands it off to your controller. That way, almost
+// all of the full stack is used, and your code will be
+// called in exactly the same way as if it were processing
+// a real HTTP request but without the cost of starting the server.
     @Autowired
     private MockMvc mockMvc;
 
     @Test
     void shouldReturnString() throws Exception {
-        this.mockMvc.perform(get("/books"))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(content().string(containsString("bookstore")));
+        //given
+        String htmlString = "bookstore";
 
+        //when
+        ResultActions resultActions = this.mockMvc.perform(get("/books"))
+                .andDo(print());
+
+        //then
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString(htmlString)));
     }
 }
